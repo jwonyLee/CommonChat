8795443b94d2827e1964a76a6552568369ae4bed
JiWon Lee <jiwony999@gmail.com>
2018-05-01 13:01:36 +0900

    modify

--- app/src/main/java/com/example/jiwon/commonchat/MainActivity.java
+++ app/src/main/java/com/example/jiwon/commonchat/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, JoinUsActivity.class);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
 
